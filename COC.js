5a90363acd5ba44b902bfac4f785ebea7541e280
Adnan Dakhani <dakhani.an01@gmail.com>
2023-06-18 11:02:58 +0530

    Committed

---
+++ COC.js
@@ -0,0 +1,10 @@
+const PI = 3.14;
+let radius
+let circumference;
+
+radius = prompt("enter the radius");
+radius = Number(radius);
+
+circumference = 2 * PI * radius;
+
+console.log("Circumference of circle is", circumference);
